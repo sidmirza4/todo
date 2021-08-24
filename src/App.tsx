3cc25567d028cbe44f4abf5fa925c98522b22014
Mohd Shahid <sidmirza4@gmail.com>
2021-08-24 20:53:58 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-function App() {
+import React, { useState } from 'react';
+
+const App = () => {
+	const [theme, setTheme] = useState('light');
+	const isDarkTheme = theme === 'dark';
+
+	const toggleTheme = () => setTheme(isDarkTheme ? 'light' : 'dark');
+
 	return <div className='App'></div>;
-}
+};
 
 export default App;
